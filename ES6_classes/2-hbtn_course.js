2496a2c2b4564dc2d1bdb78c4e697959ef050794
gofastpanam <8972@holbertonstudents.com>
2024-07-20 20:41:00 +0200

    2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,7 @@
+export default class HolbertonCourse {
+    constructor(name, lenght, students) {
+        this._name = name;
+        this._lenght = lenght;
+        this._students = students;
+    }
+}
