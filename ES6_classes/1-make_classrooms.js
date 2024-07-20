3c6035d89f429df6affdc393fcd310453a48b0b0
gofastpanam <8972@holbertonstudents.com>
2024-07-20 20:34:16 +0200

    1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom";
+
+
+export default function initializeRooms() {
+    const roomSizes = [19, 20, 34];
+    const rooms = roomSizes.map(size => new ClassRoom(size));
+    return rooms;
+}
