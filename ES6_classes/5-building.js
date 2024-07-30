b0f64326a921711e0018a386e7d3204dd9494eb5
gofastpanam <8972@holbertonstudents.com>
2024-07-30 11:17:27 +0200

    5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
