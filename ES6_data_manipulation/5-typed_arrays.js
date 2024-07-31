024e08a6d02f829fa3696673df64b3ae1e3d888e
gofastpanam <8972@holbertonstudents.com>
2024-07-31 10:31:12 +0200

    5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+// returns a new ArrayBuffer with an Int8 value.
+
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+  return view;
+}
+
+export default createInt8TypedArray;
