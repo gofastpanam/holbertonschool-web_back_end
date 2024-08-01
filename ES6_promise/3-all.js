06a61b6b81aa556bbd02a549b02a7e3d2abefe4b
gofastpanam <8972@holbertonstudents.com>
2024-08-01 15:14:41 +0200

    3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+// Handle multiple successful promises
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
