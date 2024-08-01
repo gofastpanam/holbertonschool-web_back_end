d6160a32625b3efd13c6dad966d9e16a4ccc3e53
gofastpanam <8972@holbertonstudents.com>
2024-08-01 15:10:34 +0200

    2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+// Append three handlers to the function
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
