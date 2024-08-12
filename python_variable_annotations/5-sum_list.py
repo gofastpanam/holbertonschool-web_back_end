219333ba89a7023daf38412af7c9692269842c5e
gofastpanam <8972@holbertonstudents.com>
2024-08-12 18:14:51 +0200

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -6,7 +6,7 @@ and returns their sum as a float
 """
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: list[float]) -> float:
     """
     This function takes
     a list input_list of floats as argument
