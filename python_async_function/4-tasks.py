a4570fab1116fd847c72feaee80d87da52c8aecc
gofastpanam <8972@holbertonstudents.com>
2024-08-20 11:18:26 +0200

    4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay
+task_wait_random is task beign called
+"""
+import asyncio
+import random
+from typing import List
+
+wait_r = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the specified max_delay.
+    Return the list of all the delays in ascending order.
+    """
+    delays = await asyncio.gather(*(wait_r(max_delay) for i in range(n)))
+    sorted_delays = []
+    for delay in delays:
+        inserted = False
+        for i in range(len(sorted_delays)):
+            if delay < sorted_delays[i]:
+                sorted_delays.insert(i, delay)
+                inserted = True
+                break
+        if not inserted:
+            sorted_delays.append(delay)
+    return sorted_delays
