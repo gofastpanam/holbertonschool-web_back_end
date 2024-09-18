a6ba6242d59f9b676b841c29be0d5a6347613856
gofastpanam <8972@holbertonstudents.com>
2024-09-18 12:07:30 +0200

    6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
     res.send('Hello Holberton School!');
 });
 
